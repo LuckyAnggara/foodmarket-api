a0bb683810f5a5886d88bcd104b23106cb50afa9
lucky <anggara.lucky1992@gmail.com>
2022-03-21 15:32:23 +0700

    asdasdasda

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -7,13 +7,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Food extends Model
+class Transaction extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'name','description','ingredients','price','rate','types'
+        'food_id','user_id','quantity','total','status','payment_url'
     ];
+
+    public function food(){
+        return $this->hasOne(Food::class, 'id','food_id');
+    }
+
+    public function user(){
+        return $this->hasOne(User::class, 'id','user_id');
+    }
 
     public function getCreatedAtAttribute($value){
         return Carbon::parse($value)->timestamp();
@@ -23,14 +31,5 @@ class Food extends Model
         return Carbon::parse($value)->timestamp();
     }
 
-    public function toArray(){
-        $toArray = parent::toArray();
-        $toArray['picturePath'] = $this->picturePath;
-        return $toArray;
-    }
-
-    public function getPicturePathAttribute(){
-        return url('') . Storage::url($this->attributes['picturePath']);
-    }
 
 }
