f80845b75cb510ae3773db21e251c979b85e7b63
lucky <anggara.lucky1992@gmail.com>
2022-03-22 15:30:38 +0700

    asdasd

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -115,5 +116,31 @@ class UserController extends Controller
         $user->update($data);
 
         return ResponseFormatter::success($user,'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'file'=> 'required|image|max:2048'
+        ]);
+        
+        if($validator->fails()){
+            return ResponseFormatter::error([
+                'error'=>$validator->errors()
+            ],'Update photo fails', 401);
+        }
+        if($request->file('file'))
+        {
+            $file = $request->file->store('assets/user','public');
+            // Simpan Foto ke Database
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file],'File successfully uploaded'); 
+        }
+
+
+
     }
 }
